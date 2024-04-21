00a8a27a7a57ffd62fa01b82bd6dbeb7297dca3a
Fabian Lange <fabian.lange@srccast.de>
2024-04-21 15:43:49 +0200

    Add decorator.py

---
+++ management_command_decorator/decorator.py
@@ -0,0 +1,16 @@
+from django.core.management import BaseCommand
+
+command_dict = {}
+
+
+class mgmt_command(BaseCommand):  # noqa: N801
+    def __init__(self, wrapped):
+        super().__init__()
+        self.wrapped = wrapped
+        command_dict[wrapped.__name__] = self
+
+    def __call__(self, *args, **kwargs):
+        return self.wrapped(*args, **kwargs)
+
+    def handle(self, *args, **options):
+        return self()
