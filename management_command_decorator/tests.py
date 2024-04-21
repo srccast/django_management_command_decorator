ca02558acc18ae5ea80b71c9e91173390bf8453c
Fabian Lange <fabian.lange@srccast.de>
2024-04-21 17:44:01 +0200

    Add tests

--- management_command_decorator/tests.py
+++ management_command_decorator/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.core.management import call_command
 
-# Create your tests here.
+from management_command_decorator import print_command, print_command_2
+
+
+def test_call_command():
+    call_command("print_command")
+
+
+def test_call_other_command():
+    call_command("other_command")
+
+
+def test_print_command_as_func():
+    # test we don't crash
+    print_command()
+    print_command_2()
