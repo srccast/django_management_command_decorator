325854c0974b1544abc05a0a0d3ecd0ecccabc13
Fabian Lange <fabian.lange@srccast.de>
2024-04-21 15:43:38 +0200

    Update apps.py

--- management_command_decorator/apps.py
+++ management_command_decorator/apps.py
@@ -1,6 +1,23 @@
+import django
 from django.apps import AppConfig
+
+from management_command_decorator.decorator import command_dict
 
 
 class ManagementCommandDecoratorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'management_command_decorator'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "management_command_decorator"
+
+    def ready(self):
+        from django.core.management import get_commands
+
+        old_get_commands = get_commands
+
+        def new_get_commands(*args, **kwargs):
+            commands = old_get_commands(*args, **kwargs)
+            for name, cmd in command_dict.items():
+                commands[name] = cmd
+
+            return commands
+
+        django.core.management.get_commands = new_get_commands
