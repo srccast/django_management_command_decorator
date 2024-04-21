cc5b43a7d308bb20695da3a8068c76c57a82ee5a
Fabian Lange <fabian.lange@srccast.de>
2024-04-21 15:39:46 +0200

    Add project files

---
+++ management_command_decorator/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ManagementCommandDecoratorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'management_command_decorator'
