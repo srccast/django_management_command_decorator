86f7d8b199dbe71c06dc418a203335f084659737
Fabian Lange <fabian.lange@srccast.de>
2024-04-21 17:09:36 +0200

    Use wrapt for decorator logic

--- management_command_decorator/decorator.py
+++ management_command_decorator/decorator.py
@@ -1,19 +1,12 @@
-from django.core.management import BaseCommand
+import wrapt
 
 command_dict = {}
 
 
-class mgmt_command(BaseCommand):  # noqa: N801
-    def __init__(self, wrapped):
-        super().__init__()
-        self.wrapped = wrapped
-        command_dict[wrapped.__name__] = self
+def django_management_command(name=None):
+    @wrapt.decorator
+    def wrapper(wrapped, instance, args, kwargs):
+        return wrapped(*args, **kwargs)
 
-    def __call__(self, *args, **kwargs):
-        return self.wrapped(*args, **kwargs)
-
-    def handle(self, *args, **options):
-        return self()
-
-    def rpartition(self, *args, **kwargs):
-        return [self.wrapped.__module__.rpartition(".")[0]]
+    command_dict[name if name else wrapper.__name__] = wrapper
+    return wrapper
