0b619500abcde2fe02512380e5dc3e83b9a272fc
Fabian Lange <fabian.lange@srccast.de>
2024-04-21 17:38:10 +0200

    Rewrite django_management_command

--- management_command_decorator/decorator.py
+++ management_command_decorator/decorator.py
@@ -1,12 +1,31 @@
+import functools
+
+from django.core.management import BaseCommand
+
 import wrapt
 
 command_dict = {}
 
 
-def django_management_command(name=None):
+class DecoratedCommand(BaseCommand):
+    def __init__(self, func, *args, **kwargs):
+        self.func = func
+        super().__init__(*args, **kwargs)
+
+    def handle(self, *args, **options):
+        return self.func()
+
+    def rpartition(self, *args, **kwargs):
+        return [self.func.__module__.rpartition(".")[0]]
+
+
+def django_management_command(wrapped=None, *, name=None):
+    if wrapped is None:
+        return functools.partial(django_management_command, name=name)
+
     @wrapt.decorator
     def wrapper(wrapped, instance, args, kwargs):
         return wrapped(*args, **kwargs)
 
-    command_dict[name if name else wrapper.__name__] = wrapper
+    command_dict[name if name else wrapped.__name__] = DecoratedCommand(wrapped)
     return wrapper
