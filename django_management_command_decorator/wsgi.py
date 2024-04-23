4a0eac4b9c10528b233c78895e506e8f376946d0
Fabian Lange <fabian.lange@srccast.de>
2024-04-23 17:44:21 +0200

    Lint files

--- django_management_command_decorator/wsgi.py
+++ django_management_command_decorator/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_management_command_decorator.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_management_command_decorator.settings")
 
 application = get_wsgi_application()
